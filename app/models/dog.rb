0ed2a62743acc43a95c1af37e97d327f97011dfd
James Lenahan <james.k.lenahan@gmail.com>
2020-02-06 18:07:49 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,16 @@
 class Dog
-  attr_accessor :name, :breed, :age
-  @all = []
-  def initialize
+  attr_reader :breed
+  attr_accessor :name, :age
+  
+  @@dogs = []
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@dogs << self
+  end
+  
+  def self.all
+    @@dogs
   end
 end
